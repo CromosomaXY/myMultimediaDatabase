53028d87509124da2d5497cd69a321b7f9d2ee1c
AlfonsoLazaro <pisha666@gmail.com>
2018-09-25 15:02:07 +0200

    beans

--- app/src/main/java/com/elterabit/mymultimediadatabase/Constantes.java
+++ app/src/main/java/com/elterabit/mymultimediadatabase/Constantes.java
@@ -16,9 +16,24 @@ public class Constantes {
     //TABLA VIDEOJUEGOS
     public static final String TABLA_VIDEOJUEGOS="videojuegos";
     public static final String CAMPO_ID_VIDEOJUEGO="id";
+    public static final String CAMPO_NOMBRE_VIDEOJUEGO="nombre";
+    public static final String CAMPO_NOMBRE_PLATAFORMA="plataforma";
+    public static final String CAMPO_NUMERO_JUGADORES="jugadores";
+    public static final String CAMPO_GENERO="genero";
+    public static final String CAMPO_DESARROLLADOR="desarrollador";
+    public static final String CAMPO_FORMATO="formato";
+    public static final String CREAR_TABLA_VIDEOJUEGOS="CREATE TABLE " +
+            "" +TABLA_VIDEOJUEGOS+" ("+CAMPO_ID_VIDEOJUEGO+" INTEGER, " +
+                                     ""+CAMPO_NOMBRE_VIDEOJUEGO+" TEXT, " +
+                                     ""+CAMPO_NOMBRE_PLATAFORMA+" TEXT, " +
+                                     ""+CAMPO_NUMERO_JUGADORES+" TEXT, " +
+                                     ""+CAMPO_GENERO+" TEXT, " +
+                                     ""+CAMPO_DESARROLLADOR+" TEXT, " +
+                                     ""+CAMPO_FORMATO+" TEXT)";
 
     //TABLA PELICULAS
     public static final String TABLA_PELICULAS="peliculas";
+
 
     //TABLA MUSICA
     public static final String TABLA_MUSICA="musica";
