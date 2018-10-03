574f70e2754ae4fc233b7d4fc7b5f134a27749ec
CromosomaXY <pisha666@gmail.com>
2018-10-03 19:31:58 +0200

    generadores PK

--- app/src/main/java/com/elterabit/mymultimediadatabase/Util.java
+++ app/src/main/java/com/elterabit/mymultimediadatabase/Util.java
@@ -7,5 +7,10 @@ public class Util {
     public String generadorPkVideojuegos(){
         return "VJ-" + UUID.randomUUID().toString();
     }
+    public String generadorPkSistemas() { return "SYS-" + UUID.randomUUID().toString();}
+    public String generadorPkPeliculas() { return "MV-" + UUID.randomUUID().toString();}
+    public String generadorPkMusica() { return "MS-" + UUID.randomUUID().toString();}
+    public String generadorPkLibros() { return  "LB-" + UUID.randomUUID().toString();}
+    public String generadorPkComics() { return "CM-" + UUID.randomUUID().toString();}
 
 }
