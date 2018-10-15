fa540d0f647e3230f8e721d3a534da5b683779a6
AlfonsoLazaro <pisha666@gmail.com>
2018-10-15 16:44:53 +0200

    altas y detalles

--- app/src/main/java/com/elterabit/detalles/DetallePeliculas.java
+++ app/src/main/java/com/elterabit/detalles/DetallePeliculas.java
@@ -2,13 +2,24 @@ package com.elterabit.detalles;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.elterabit.mymultimediadatabase.R;
 
 public class DetallePeliculas extends AppCompatActivity {
 
+    TextView idPelicula, nombrePelicula, nombreProductora, annioPelicula, formatoPelicula;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_peliculas);
+
+        idPelicula = findViewById(R.id.campoIdPelicula);
+        nombrePelicula = findViewById(R.id.campoNombrePelicula);
+        nombreProductora = findViewById(R.id.campoNombreProductora);
+        annioPelicula = findViewById(R.id.campoAnnioPelicula);
+
+
+
     }
 }
